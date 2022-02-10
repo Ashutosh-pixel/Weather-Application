02f9f902107e470c350a71ec430b040eda98e815
Ashutosh <ashutoshsharma222199@gmail.com>
2022-02-10 19:28:37 +0530

    Back-End Working

--- main.py
+++ main.py
@@ -56,7 +56,46 @@ class MyWeather:
         self.lbl_Error = Label(self.root,font=("Ubuntu",17),bg="white",fg="red")
         self.lbl_Error.place(x=150,y=380,height=50)
 
-    
+    def get_weather(self):
+            Api_key=API.Api_key
+            complete_url=f"http://api.openweathermap.org/data/2.5/weather?q={self.var_search.get()}&appid={Api_key}"
+            if self.var_search.get() == "":
+                self.lbl_city.config(text="")
+                self.lbl_icons.config(image="")
+                self.lbl_temp.config(text="")
+                self.lbl_wind.config(text="")
+                self.lbl_Error.config(text="Invalid City")
+                self.lbl_Error.config(text="City Name Required")
+            else:
+                result=requests.get(complete_url)
+                if result :
+                    json=result.json() 
+                    city_name=json["name"]
+                    country=json["sys"]["country"]
+                    icons=json["weather"][0]["icon"]
+                    temp_c=json["main"]["temp"]-273.15
+                    temp_f=(json["main"]["temp"]-273.15)* 9/5 +32
+                    wind=json["weather"][0]["main"]
+                    self.lbl_city.config(text=city_name+" , "+country)
+
+                    #=====NEW ICONS==========================
+                    self.search_icon2 = Image.open(f"icons/{icons}.png")
+                    self.search_icon2 = self.search_icon2.resize((100,100),Image.ANTIALIAS)
+                    self.search_icon2 = ImageTk.PhotoImage(self.search_icon2)
+
+
+                    self.lbl_icons.config(image=self.search_icon2)
+                    deg=u"\N{DEGREE SIGN}"
+                    self.lbl_temp.config(text=str(round(temp_c,2))+deg+"C |"+str(round(temp_f,2))+deg+"f")
+                    self.lbl_wind.config(text=wind)
+                    self.lbl_Error.config(text="")
+                    
+                else:
+                    self.lbl_city.config(text="")
+                    self.lbl_icons.config(image="")
+                    self.lbl_temp.config(text="")
+                    self.lbl_wind.config(text="")
+                    self.lbl_Error.config(text="Invalid City Name")
 
 
 root = Tk() 
